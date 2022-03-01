ee9d70186b21e96139ccb5c99791a55117ad3313
vinothsha <74014926+vinothsha@users.noreply.github.com>
2022-03-01 12:59:51 +0530

    Add files via upload

---
+++ fordeploy.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "hello AWS ec2 instance ")
+}
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":5000", nil)
+}
